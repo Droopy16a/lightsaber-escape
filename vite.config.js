50a546c11c8cf83573070effb0ceb760d4801f18
droopy13a <gaetancozien07@gmail.com>
2025-07-07 16:08:11 +0200

    app deploy

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,9 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // nom du repo GitHub,
+  base: '/lightsaber-escape/', // nom du repo GitHub,
   server: {
     host: '192.168.1.19',
-    port: 3000,
-    allowedHosts: ['a6cc-2a01-cb00-18c7-a00-e5bd-ab4f-c883-93d9.ngrok-free.app']
+    port: 3000
   }
 })
